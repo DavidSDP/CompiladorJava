1e2f263b7c3c12590a9371ecf6c698fd615e9ec7
DavidSDP <davidsahuquillodepablos@gmail.com>
2020-03-02 21:28:17 +0100

    Corregida Gramática con arrays

---
+++ src/SimbolosNoTerminales/SimboloDeclaracion.java
@@ -0,0 +1,13 @@
+package SimbolosNoTerminales;
+
+import Checkers.Tipo;
+import Procesador.TipoSubyacente;
+import analisisSintactico.arbol.Nodo;
+
+public class SimboloDeclaracion extends Nodo implements TipoSubyacente{
+
+	@Override
+	public Tipo getTipoSubyacente() {
+	}
+
+}
---
+++ src/SimbolosNoTerminales/SimboloInicializacion.java
@@ -0,0 +1,5 @@
+package SimbolosNoTerminales;
+
+public class SimboloInicializacion {
+
+}
